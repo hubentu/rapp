4a056fd4f7827e5e78dd7e9954134ebec9b4bc47
hubentu <huqmail@gmail.com>
2019-11-16 21:16:55 -0500

    Add main test

---
+++ tests/testthat/main.R
@@ -0,0 +1,22 @@
+
+ui <- c(
+    vtags$v_navigation_drawer(props = c("v-model" = "drawer",
+                                        ":clipped"="$vuetify.breakpoint.lgAndUp",
+                                        app = TRUE)),
+    vtags$v_app_bar(props = c(":clipped-left" = "$vuetify.breakpoint.lgAndUp",
+                              color = "blue darken-3",
+                              app = TRUE,
+                              dark = TRUE), list(
+                                                vtags$v_app_bar_nav_icon(props = c("@click.stop" = "drawer = !drawer")),
+                                                vtags$v_toolbar_title("Contacts"), 
+                                                vtags$v_text_field(props = c(flat = TRUE,
+                                                                             "hide-details" = TRUE,
+                                                                             "prepend-inner-icon" = TRUE,
+                                                                             label = "search")
+                                                                   )
+                                            )
+                    )
+)
+
+ui <- BuildUI(uid = "header", ui)
+renderUI(ui, dataList = list(drawer = ""), outdir = "~/Downloads/test")
